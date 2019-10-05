da129a4a410807451c03d31203ca9a6cb24963c7
MikeG212 <michaelcgee93@gmail.com>
2019-10-04 17:21:37 -0700

    Add meta tags and header

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c765747d9af266a5f45f";
+/******/ 	var hotCurrentHash = "b424a2c107d03f64948c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,9 +3,6 @@ import Link from "next/link";
 const Home = props => (
   <div>
     <p>Welcome to SlickSporting</p>
-    <Link href="/sell">
-      <a>Sell</a>
-    </Link>
   </div>
 );
 
