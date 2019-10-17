6f2276c52efa3b07ed22778bccfa43a44e97935f
MikeG212 <michaelcgee93@gmail.com>
2019-10-17 14:18:40 -0700

    Show current user's name in nav bar

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3e5ecc03b43835392b94";
+/******/ 	var hotCurrentHash = "ebfeffac02813e54ddb2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
